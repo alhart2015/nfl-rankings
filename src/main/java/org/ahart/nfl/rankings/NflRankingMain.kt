503d144c3449fa11be34f2b052164d631e57673f
alhart2015 <alhart@davidson.edu>
2018-02-14 17:58:47 -0500

    Initial commit

---
+++ src/main/java/org/ahart/nfl/rankings/NflRankingMain.kt
@@ -0,0 +1,8 @@
+package org.ahart.nfl.rankings
+
+class NflRankingMain {
+
+    fun main(args: Array<String>) {
+        println("Hello World")
+    }
+}
---
+++ src/test/java/org/ahart/nfl/rankings/TestNflRankingMain.kt
@@ -0,0 +1,13 @@
+package org.ahart.nfl.rankings
+
+import org.junit.Test
+
+class TestNflRankingMain {
+
+    @Test
+    fun runMain() {
+        var driver = NflRankingMain()
+
+        driver.main(arrayOf(""))
+    }
+}
